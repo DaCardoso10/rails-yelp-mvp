0cf7d9bfe2a09f99fabf19b2e6fafce923d4dcfe
Dario Cardoso <darioferreira10@gmail.com>
2024-02-21 16:18:45 -0300

    Yelp casi

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  resources :restaurants do
+    resources :reviews, only: [:new, :create, :index]
+  end
+  root "restaurants#index"
 end
